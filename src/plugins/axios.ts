067f818189c67bd15def0d4d8fcce3de0c79610b
ZOU <253058016@qq.com>
2022-05-28 16:40:21 +0800

    Update axios.ts

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -3,8 +3,6 @@ import axios from 'axios'
 console.log('env', process.env.NODE_ENV)
 axios.defaults.headers.post.Accept = 'application/json'
 axios.defaults.headers.post['Content-Type'] = 'application/json'
-// const env = process.env.NODE_ENV === 'development' ? 'https://web3profile-api.test-pns-link.com' : 'https://api-rpc.pns.link'
-const env = process.env.NODE_ENV === 'development' ? 'https://web3profile-api.test-pns-link.com' : 'https://web3profile-api.test-pns-link.com'
-axios.defaults.baseURL = env
+axios.defaults.baseURL = process.env.VITE_API_URI || ''
 
 export default axios
