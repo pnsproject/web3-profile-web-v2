8e3d3c4e12aece9e1b5fd24adadccf2979a05621
ZOU <253058016@qq.com>
2022-05-28 16:59:38 +0800

    update config

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,8 +1,9 @@
 import axios from 'axios'
+import config from '@/state/config'
 
 console.log('env', process.env.NODE_ENV)
 axios.defaults.headers.post.Accept = 'application/json'
 axios.defaults.headers.post['Content-Type'] = 'application/json'
-axios.defaults.baseURL = process.env.VITE_API_URI || ''
+axios.defaults.baseURL = config.apiUrl as string
 
 export default axios
